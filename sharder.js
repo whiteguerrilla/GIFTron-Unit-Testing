4bb9362bd2025396ba0d7dddf07659a1b87b698f
Snow Flake <37661675+whiteguerrilla@users.noreply.github.com>
2018-12-21 13:03:06 -0600

    Create sharder.js
    An example of evenly distributing a sha1 hash across 26^2 shards

---
+++ sharder.js
@@ -0,0 +1,50 @@
+
+      "use strict";
+
+      const shards = {};
+      const samples = 26 ** 3;
+
+      for (let i = 0; i < samples; i += 1) {
+        // establish our deterministic, distributed hash
+        const hash = sha1();
+
+        // obtain the shard
+        const shard = hash2shard(sha1());
+
+        // register the shard
+        shards[shard] = (shards[shard] || 0) + 1;
+      }
+
+      // reveal the distribution
+      console.log(shards);
+
+      function hash2shard(hash) {
+        // establish our legal alphabet
+        const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        // select three characters to ensure an even distribution in 26^2 space (cuz 16^3 ~ 26^2 / 6)
+        const fragment = hash.substr(0, 3);
+
+        // convert our fragment to an integer which we can transform to suit our needs
+        const dividend = parseInt(fragment, 16);
+
+        // scale our integer down to 26^2
+        const quotient = (dividend / 6) | 0;
+
+        // convert to base 26 and pad if necessary
+        const shard = ("0" + quotient.toString(26)).substr(-2);
+
+        // shift characters to legal set
+        return shard
+          .split("")
+          .map(v => alphabet[parseInt(v, 26)])
+          .join("");
+      }
+
+      function sha1() {
+        const hash = [];
+        while (hash.length < 40) {
+          hash.push(((Math.random() * 16) | 0).toString(16));
+        }
+        return hash.join("");
+      }
