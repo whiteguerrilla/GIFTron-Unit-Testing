cd1d9de3590ea2dd654b40ed7976d51c1f6d82ae
Snow Flake <37661675+whiteguerrilla@users.noreply.github.com>
2018-12-21 20:20:30 -0600

    Improve distribution by expanding fragment to six characters

--- sharder.js
+++ sharder.js
@@ -1,7 +1,7 @@
       "use strict";
 
       const shards = {};
-      const samples = 26 ** 3;
+      const samples = 26 ** 5;
 
       for (let i = 0; i < samples; i += 1) {
         // establish our deterministic, distributed hash
@@ -15,20 +15,20 @@
       }
 
       // reveal the distribution
-      console.log(shards);
+      console.log(Object.keys(shards).length, shards);
 
       function hash2shard(hash) {
         // establish our legal alphabet
         const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
         // select three characters to ensure an even distribution in 26^2 space (cuz 16^3 ~ 26^2 / 6)
-        const fragment = hash.substr(0, 3);
+        const fragment = hash.substr(0, 6);
 
         // convert our fragment to an integer which we can transform to suit our needs
-        const integer = parseInt(fragment, 16);
+        const integer = parseInt(fragment, 16) * 439;
 
         // constrain our integer to 26^2
-        const remainder = integer % 676;
+        const remainder = ((integer / 311) | 0) % 676;
 
         // convert to base 26 and pad if necessary
         const shard = ("0" + remainder.toString(26)).substr(-2);
