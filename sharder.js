31fd23f6c3dceb5901f083117a73320ff5d38540
Snow Flake <37661675+whiteguerrilla@users.noreply.github.com>
2018-12-23 00:29:40 -0600

    Expanded base from 26 to 650

--- sharder.js
+++ sharder.js
@@ -1,7 +1,8 @@
       "use strict";
 
       const shards = {};
-      const samples = 26 ** 5;
+      const base = getBase();
+      const samples = base.length ** 2;
 
       for (let i = 0; i < samples; i += 1) {
         // establish our deterministic, distributed hash
@@ -18,26 +19,17 @@
       console.log(Object.keys(shards).length, shards);
 
       function hash2shard(hash) {
-        // establish our legal alphabet
-        const alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-        // select three characters to ensure an even distribution in 26^2 space (cuz 16^3 ~ 26^2 / 6)
-        const fragment = hash.substr(0, 6);
+        // select four characters to ensure an even distribution in 26^2 - 26 space
+        const fragment = hash.substr(0, 4);
 
         // convert our fragment to an integer which we can transform to suit our needs
-        const integer = parseInt(fragment, 16) * 439;
+        const integer = parseInt(fragment, 16) * 131;
 
-        // constrain our integer to 26^2
-        const remainder = ((integer / 311) | 0) % 676;
+        // constrain our integer to 26^2 - 26
+        const remainder = ((integer / 127) | 0) % 650;
 
-        // convert to base 26 and pad if necessary
-        const shard = ("0" + remainder.toString(26)).substr(-2);
-
-        // shift characters to legal set
-        return shard
-          .split("")
-          .map(v => alphabet[parseInt(v, 26)])
-          .join("");
+        // return our base 26^2 - 26 representative
+        return base[remainder];
       }
 
       function sha1() {
@@ -46,4 +38,21 @@
           hash.push(((Math.random() * 16) | 0).toString(16));
         }
         return hash.join("");
+      }
+
+      function getBase() {
+        // establish our legal alphabet
+        const alphabet = "abcdefghijklmnopqrstuvwxyz";
+        const limit = alphabet.length;
+        const others = limit - 1;
+        const pairs = [];
+
+        // establish our base representatives
+        for (let i = 0; i < limit; i += 1) {
+          for (let j = 0; j < others; j += 1) {
+            pairs.push(alphabet[i] + alphabet[(i + j + 1) % limit]);
+          }
+        }
+
+        return pairs;
       }
